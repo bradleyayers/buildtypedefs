5658bdd9261be18a360b7806bc39d403dbcea9ca
Bradley Ayers <bradley.ayers@gmail.com>
2017-01-04 19:17:44 +1100

    Add support for unnamed classes.

--- src/extract.ts
+++ src/extract.ts
@@ -55,8 +55,266 @@ export function extractProperty(commentPath: Path): PropertySpec | undefined {
  * Strip off the ' :: ' or ' : ' prefix.
  */
 function stripCommentSpecPrefix(prefixedSpec: string): string {
-    const rawCommentSpec = prefixedSpec;
-    const [ matched, prefix, spec] = rawCommentSpec.match(/^( ::? )?(.*)$/);
-    invariant(!!matched, `Invalid comment spec syntax '${}'.`)
-    return spec;
+  const rawCommentSpec = prefixedSpec;
+  const [matched, prefix, spec] = rawCommentSpec.match(/^( ::? )?(.*)$/);
+  invariant(!!matched, `Invalid comment spec syntax '${prefixedSpec}'.`)
+  return spec;
 }
+
+export interface Declaration {
+  name?: string;
+  typeSpec?: string;
+  properties?: Declaration[];
+}
+
+export function extract(source: string): Declaration[] {
+  const declarations = [];
+  const nodeToDeclarationMap = [] as { path: any; declaration: Declaration }[];
+  const program = j(source);
+
+  interface DeclarationLine {
+    kind: 'DeclarationLine';
+    identifier?: string;
+    indent: number;
+    typeSpec: string;
+  }
+
+  interface DocumentationLine {
+    kind: 'DocumentationLine';
+    text: string;
+    indent: number;
+  }
+
+  interface EmptyLine {
+    kind: 'EmptyLine';
+    indent: number;
+  }
+
+  type Line = DeclarationLine | DocumentationLine | EmptyLine;
+
+  gatherComments().reverse().forEach(parseComment);
+  return declarations;
+
+  function parseLine(line: string): Line {
+    let result;
+
+    // Test for a DeclarationLine.
+    result = line.match(/^( +)([a-zA-Z\._]*)(::?-? *)(.*)$/);
+    if (result) {
+      const [_, indent, identifier, colons, spec] = result;
+      const line: DeclarationLine = {
+        kind: 'DeclarationLine',
+        indent: indent.length,
+        typeSpec: spec
+      };
+      if (identifier) {
+        line.identifier = identifier;
+      }
+      return line;
+    }
+
+    // Test for a DocumentationLine.
+    result = line.match(/^( +)(.+)$/);
+    if (result) {
+      const [_, indent, text] = result;
+      return {
+        kind: 'DocumentationLine',
+        indent: indent.length,
+        text
+      }
+    }
+
+    // Test for an EmptyLine.
+    result = line.match(/^( *)$/);
+    if (result) {
+      const [_, indent] = result;
+      return {
+        kind: 'EmptyLine',
+        indent
+      };
+    }
+    throw new Error(`Unknown syntax in comment: ${line}`);
+  }
+
+  function parseComment(comments: CommentBlock) {
+    const lines = comments.lines.map(parseLine);
+    const end = lines.length;
+    let pos = 0;
+    let line = lines[pos];
+
+    while (line) {
+      switch (line.kind) {
+        case 'DeclarationLine':
+          const declarationLine = line;
+          const declaration = parseDeclaration();
+          let parentDeclaration = findParentDeclaration(comments.associatedNodePath);
+          if (!parentDeclaration) {
+            const classDeclarations = j(comments.associatedNodePath).closest(j.ClassDeclaration).paths();
+            if (classDeclarations.length === 1) {
+              const classDeclaration = classDeclarations[0];
+              parentDeclaration = {
+                name: nameFromPath(classDeclaration),
+                typeSpec: typeSpecFromPath(classDeclaration)
+              };
+              declarations.push(parentDeclaration);
+              nodeToDeclarationMap.push({ path: classDeclaration, declaration: parentDeclaration });
+            }
+          }
+
+          if (parentDeclaration) {
+            parentDeclaration.properties = parentDeclaration.properties || [];
+            parentDeclaration.properties.push(declaration);
+          } else {
+            declarations.push(declaration);
+          }
+          nodeToDeclarationMap.push({
+            path: comments.associatedNodePath,
+            declaration
+          });
+          break;
+        case 'DocumentationLine':
+        case 'EmptyLine':
+          nextLine();
+          break;
+      }
+    }
+
+    function findParentDeclaration(p: any): Declaration | undefined {
+      for (const { path, declaration } of nodeToDeclarationMap) {
+        let parent = p;
+        while (parent = parent.parentPath) {
+          if (parent === path) {
+            return declaration;
+          }
+        }
+      }
+    }
+
+    function nameFromPath(path: any): string {
+      const node = path.node;
+      switch (node.type) {
+        case 'ClassDeclaration':
+          return node.id.name;
+        case 'MethodDefinition':
+          return node.key.name;
+        default:
+          throw new Error(`Unable to derive declaration name from a '${node.type}'.`);
+      }
+    }
+
+    function typeSpecFromPath(path: any): string {
+      switch (path.value.type) {
+        case 'ClassDeclaration':
+          return 'class';
+        default:
+          throw new Error(`Unable to derive declaration type from a '${path.value.type}'.`);
+      }
+    }
+
+    function parseDeclaration(): Declaration {
+      const { typeSpec, identifier, indent } = line as DeclarationLine;
+      const declaration: Declaration = {}
+
+      // A declaration without a name implicitly applies to the next
+      // program element. We derive the name based on that.
+      declaration.name = identifier || nameFromPath(comments.associatedNodePath);
+      declaration.typeSpec = typeSpec || typeSpecFromPath(comments.associatedNodePath);
+
+      skipUntil('EmptyLine');
+
+      loop: while (line) {
+        switch (line.kind) {
+          case 'DocumentationLine':
+            skipUntil('EmptyLine');
+            break;
+          case 'EmptyLine':
+            nextLine();
+            break;
+          case 'DeclarationLine':
+            if (line.indent > indent) {
+              const properties = parseProperties();
+              if (properties.length) {
+                declaration.properties = properties;
+              }
+            }
+            break loop;
+        }
+      }
+
+      return declaration;
+    }
+
+    function parseProperties(): Declaration[] {
+      const { indent } = line;
+      const members = [];
+
+      loop: while (line) {
+        switch (line.kind) {
+          case 'EmptyLine':
+            nextLine();
+            break;
+          case 'DocumentationLine':
+            if (line.indent >= indent) {
+              throw new Error('Unexpected documentation line.');
+            }
+          case 'DeclarationLine':
+            if (line.indent === indent) {
+              members.push(parseDeclaration());
+              break;
+            } else if (line.indent < indent) {
+              break loop;
+            }
+        }
+      }
+
+      return members;
+    }
+
+    function skipUntil(kind: string) {
+      while (line && line.kind !== kind) {
+        nextLine();
+      }
+    }
+
+    function nextLine() {
+      line = pos < end
+        ? lines[++pos]
+        : undefined;
+      return line;
+    }
+  }
+
+  /**
+   * Extract groups of comments and their proceeding program element from a
+   * program.
+   *
+   * This organisation of comments is convenient to work on for extracting
+   * getdocs information.
+   */
+  function gatherComments(): CommentBlock[] {
+    const commentsPaths = [];
+
+    // The psuedo "comments" path only seems accessible via .parentPath on a
+    // Comment path, so we're left with an inefficient algorithm here of
+    // building a unique array of each Comment's .parentPath
+    program.find(j.Comment).paths().forEach(commentPath => {
+      const commentsPath = commentPath.parentPath;
+      const lastCommentsPath = commentsPaths[commentsPaths.length - 1];
+      if (commentsPath !== lastCommentsPath) {
+        commentsPaths.push(commentsPath);
+      }
+    });
+
+    return commentsPaths.map(commentsPath => {
+      const lines = j(commentsPath).find(j.Comment).nodes().map(node => node.value);
+      const associatedNodePath = commentsPath.parentPath;
+      return { lines, associatedNodePath };
+    });
+  }
+
+  interface CommentBlock {
+    lines: string[];
+    associatedNodePath: any;
+  }
+}
+
--- test/extract.ts
+++ test/extract.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import 'mocha';
 import * as j from 'jscodeshift';
-import { extractMethod, extractProperty } from '../src/extract';
+import { extractMethod, extractProperty, extract, Declaration } from '../src/extract';
 import { SpecKind, MethodSpec, PropertySpec } from '../src/types';
 
 describe('extractMethod', () => {
@@ -17,12 +17,12 @@ describe('extractMethod', () => {
       bar(a, b = 1) {}
     }
     `, {
-      kind: SpecKind.Method,
-      name: 'bar',
-      spec: '(?Object) → ContentMatch',
-      parent: 'Foo',
-      paramNames: ['a', 'b'],
-    });
+        kind: SpecKind.Method,
+        name: 'bar',
+        spec: '(?Object) → ContentMatch',
+        parent: 'Foo',
+        paramNames: ['a', 'b'],
+      });
   });
 
   it('extracts class and method name and removes spec " :: " prefix', () => {
@@ -32,12 +32,12 @@ describe('extractMethod', () => {
       bar(a, b = 1) {}
     }
     `, {
-      kind: SpecKind.Method,
-      name: 'bar',
-      spec: '(?Object) → ContentMatch',
-      paramNames: ['a', 'b'],
-      parent: 'Foo'
-    });
+        kind: SpecKind.Method,
+        name: 'bar',
+        spec: '(?Object) → ContentMatch',
+        paramNames: ['a', 'b'],
+        parent: 'Foo'
+      });
   });
 });
 
@@ -56,11 +56,11 @@ describe('extractProperty', () => {
       }
     }
     `, {
-      kind: SpecKind.Property,
-      name: 'schema',
-      spec: 'Schema',
-      parent: 'Foo'
-    });
+        kind: SpecKind.Property,
+        name: 'schema',
+        spec: 'Schema',
+        parent: 'Foo'
+      });
   });
 
   it('extracts from a constructor with " :: " prefix', () => {
@@ -72,10 +72,297 @@ describe('extractProperty', () => {
       }
     }
     `, {
-      kind: SpecKind.Property,
-      name: 'schema',
-      spec: 'Schema',
-      parent: 'Foo'
-    });
+        kind: SpecKind.Property,
+        name: 'schema',
+        spec: 'Schema',
+        parent: 'Foo'
+      });
   });
+});
+
+
+describe('extract', () => {
+  describe('with a type', () => {
+    check('declaration', `
+      // a:: foo
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'foo'
+        }
+      ]);
+
+    check('declaration, documentation', `
+      // a:: foo
+      // Some documentation
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'foo'
+        }
+      ]);
+
+    check('declaration, property', `
+      // a:: foo
+      //
+      //  b:: foo
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'foo',
+          properties: [
+            {
+              name: 'b',
+              typeSpec: 'foo'
+            }
+          ]
+        }
+      ]);
+
+    check('declaration, two properties', `
+      // a:: foo
+      //
+      //  b:: foo
+      //
+      //  c:: foo
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'foo',
+          properties: [
+            {
+              name: 'b',
+              typeSpec: 'foo'
+            },
+            {
+              name: 'c',
+              typeSpec: 'foo'
+            }
+          ]
+        }
+      ]);
+
+    check('declaration, property, property', `
+      // a:: foo
+      //
+      //  b:: foo
+      //
+      //   c:: foo
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'foo',
+          properties: [
+            {
+              name: 'b',
+              typeSpec: 'foo',
+              properties: [
+                {
+                  name: 'c',
+                  typeSpec: 'foo'
+                }
+              ]
+            }
+          ]
+        }
+      ]);
+  });
+
+  describe('interfaces from a comment', () => {
+    check('declaration', `
+      // a:: interface
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'interface'
+        }
+      ]);
+
+    check('declaration, documentation', `
+      // a:: interface
+      // Some documentation
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'interface'
+        }
+      ]);
+
+    check('declaration, documentation, empty', `
+      // a:: interface
+      // Some documentation
+      //
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'interface'
+        }
+      ]);
+
+    check('declaration, documentation, empty, documentation', `
+      // a:: interface
+      // Some documentation
+      //
+      // Some documentation
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'interface'
+        }
+      ]);
+
+    check('declaration, documentation', `
+      // a:: interface
+      // Some documentation
+      // a:: foo
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'interface'
+        }
+      ]);
+
+    check('declaration, empty, declaration', `
+      // a:: interface
+      //
+      // b:: interface
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'interface'
+        },
+        {
+          name: 'b',
+          typeSpec: 'interface'
+        }
+      ]);
+
+    check('declaration with a property', `
+      // a:: interface
+      //
+      //  b:: interface
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'interface',
+          properties: [
+            {
+              name: 'b',
+              typeSpec: 'interface'
+            }
+          ]
+        }
+      ]);
+  });
+
+  describe('erroneously', () => {
+    it('throws an error when no name can be derived', () => {
+      expect(() => extract('// ::')).to.throw(Error);
+    });
+
+    it('throws an error when no type can be derived', () => {
+      expect(() => extract('// foo::-')).to.throw(Error);
+    });
+  })
+
+  describe('for a class', () => {
+    check('class name', `
+      // ::-
+      class Foo {}
+      `, [
+        {
+          name: 'Foo',
+          typeSpec: 'class'
+        }
+      ]);
+
+    check('explicit name trumps implied name', `
+      // a::-
+      class Foo {}
+      `, [
+        {
+          name: 'a',
+          typeSpec: 'class'
+        }
+      ]);
+
+    check('method of declared class', `
+      // ::-
+      class Foo {
+        // :: (?Object) → ContentMatch
+        bar(a, b = 1) {}
+      }
+      `, [
+        {
+          name: 'Foo',
+          typeSpec: 'class',
+          properties: [
+            {
+              name: 'bar',
+              typeSpec: '(?Object) → ContentMatch',
+            }
+          ]
+        }
+      ]);
+
+    check('method of undeclared class', `
+      class Foo {
+        // :: (?Object) → ContentMatch
+        bar(a, b = 1) {}
+      }
+      `, [
+        {
+          name: 'Foo',
+          typeSpec: 'class',
+          properties: [
+            {
+              name: 'bar',
+              typeSpec: '(?Object) → ContentMatch',
+            }
+          ]
+        }
+      ]);
+
+    check('property of declared class', `
+      // ::-
+      class Foo {
+        // bar:: foo
+      }
+      `, [
+        {
+          name: 'Foo',
+          typeSpec: 'class',
+          properties: [
+            {
+              name: 'bar',
+              typeSpec: 'foo',
+            }
+          ]
+        }
+      ]);
+
+    check('property of an undeclared class', `
+      class Foo {
+        // bar:: foo
+      }
+      `, [
+        {
+          name: 'Foo',
+          typeSpec: 'class',
+          properties: [
+            {
+              name: 'bar',
+              typeSpec: 'foo',
+            }
+          ]
+        }
+      ]);
+  });
+
+
+  function check(description: string, source: string, expected: Declaration[]) {
+    it(description, () => {
+      expect(extract(source)).to.deep.equal(expected);
+    });
+  }
 });
