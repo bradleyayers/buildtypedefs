29e8274e1a9e89bfa574ce190b8a26d212170cf3
Bradley Ayers <bradley.ayers@gmail.com>
2017-05-30 10:33:57 +1000

    Remove declare module from output.

--- src/genmodule.ts
+++ src/genmodule.ts
@@ -1,5 +1,4 @@
 import StringBuilder = require('string-builder');
-//import StringBuilder = require('string-builder');
 import {ModuleContents} from "./types"
 import {GenEnv, Imports, TypeInfos, baseTypes, mergeTypeInfos} from "./env"
 import {itemDef} from "./gendeclaration";
@@ -12,20 +11,18 @@ export default function (module: ModuleContents, name: string, typeInfos: TypeIn
   const items = module.items || {};
   const env = new GenEnv(name, imports, typeInfos, new StringBuilder(""));
 
-  env.append("declare module \"" + name + "\" {");
-
-  let indented = env.indent()
-  for (let item in items) {
-    indented.appendLine("");
-    indented.append("export ")
-    itemDef(indented, items[item], item);
-  }
-
-  env.appendLine("}");
+  Object.keys(items).forEach((item, index) => {
+    if (index > 0) {
+      env.appendLine("");
+    }
+    env.append("export ")
+    itemDef(env, items[item], item);
+    env.append(";");
+  });
 
   let importStr: string = ""
   for(let moduleName of Object.keys(imports).sort()) {
-    importStr += "import { " + imports[moduleName].sort().join(', ') + " } from '" + moduleName + "'\r\n"
+    importStr += "import { " + imports[moduleName].sort().join(', ') + " } from '" + moduleName + "';\r\n"
   }
   if (importStr != "") importStr += "\r\n"
 
--- test/module.spec.ts
+++ test/module.spec.ts
@@ -8,20 +8,20 @@ describe('when adding module definition', () => {
   it('should create an empty module', () => {
     let module = {};
     let sb = moduleDef(module, "module1", {});
-    sb.toString().should.equal('declare module "module1" {' + cr + '}')
+    sb.toString().should.equal('')
   });
 
   it('should create an module with one item', () => {
     let module = { items: {Class1: { type: "class"}} };
     let sb = moduleDef(module, "module1", {});
-    sb.toString().should.equal('declare module "module1" {' + cr + '  export class Class1 {' + cr + '  }' + cr + '}')
+    sb.toString().should.equal('export class Class1 {' + cr + '};')
   });
 
   it('should replace additional type', () => {
     let module = { items: { RedNode: { type: "class", extends: { type: "Node" } }} };
     let additionalTypes = { "Node": { replaceBy: "ProsemirrorNode", definedIn: "prosemirror-model" }};
     let sb = moduleDef(module, "module1", additionalTypes);
-    sb.toString().should.equal('import { ProsemirrorNode } from \'prosemirror-model\'' + cr + cr + 'declare module "module1" {' + cr + '  export class RedNode extends ProsemirrorNode {' + cr + '  }' + cr + '}')
+    sb.toString().should.equal('import { ProsemirrorNode } from \'prosemirror-model\';' + cr + cr + 'export class RedNode extends ProsemirrorNode {' + cr + '};')
   });
 
 });
